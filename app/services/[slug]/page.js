aeb3b4e1411644e2f392158603f51838ab31bada
swapnil9340 <swapnilchauhan08@gmail.com>
2026-05-26 16:41:34 +0530

    asdadas

--- app/services/[slug]/page.js
+++ app/services/[slug]/page.js
@@ -4,7 +4,7 @@ import { services, getServiceBySlug } from "../../data/services";
 import { cities } from "../../data/cities";
 
 const SITE_URL = "https://www.safecompanion.in";
-const whatsappNumber = "not avalible ";
+const whatsappNumber = "9340595938";
 const whatsappLink = `https://wa.me/91${whatsappNumber}?text=Hello%2C%20I%20am%20interested%20in%20your%20premium%20companion%20service.`;
 const telegramLink = `https://t.me/+91${whatsappNumber}`;
 const callLink = `tel:+91${whatsappNumber}`;
